5bff3ec299973a93ab8b700c4e5a2eeec5944700
jsixtvf <jsixtvf@hotmail.com>
2017-06-01 01:16:48 +0200

    Update jardines.js

--- js/jardines.js
+++ js/jardines.js
@@ -1,43 +1,43 @@
-var ncargas=0;
+      var ncargas=0;
 
-$(document).ready(function(){
+      $(document).ready(function(){
                   
-    
-    $(window).scroll(function(){
-        
-        if($(window).scrollTop()+$(window).height() > $(document).height()-90 && ncargas<2){
-          /*alert("FUNCIONA");*/
-           pintarJSON();
-           ncargas++;
-           }
-    
+
+        $(window).scroll(function(){
+
+            if($(window).scrollTop()+$(window).height() > $(document).height()-90 && ncargas<2){
+              /*alert("FUNCIONA");*/
+              /* pintarJSON();*/
+              $(function pintarJSON(){
+              $.getJSON( "https://cdn.rawgit.com/jsixtvf/jardines/8a6699df/json/jardines.json", function( jsonObject ) {
+              ponerJardines( jsonObject );
+              });
+              });
+               ncargas++;
+               }
+
+        });
+
     });
-    
-});
 
-$(function pintarJSON(){
-    $.getJSON( "https://cdn.rawgit.com/jsixtvf/jardines/8a6699df/json/jardines.json", function( jsonObject ) {
-        ponerJardines( jsonObject );
-    });
 
-});
 
-function ponerJardines(json){
-     $.each( json, function( i, jardines ) {
-         $('#vacio').append( $('<div>',
-  { id: '"jardines"+ncargas+."json"', text: jardines.titulo+" "+jardines.foto+" "+jardines.descripcion+" "+jardines.fecha }));
-         
-     }); 
-}
-                            
- 
-    
-$("ji").click(function() {
-  window.location = $(this).find("a").attr("href"); 
-  return false;
-});
-    
-$("jr").click(function() {
-  window.location = $(this).find("a").attr("href"); 
-  return false;
-});
+      function ponerJardines(json){
+           $.each( json, function( i, jardines ) {
+               $('#vacio').append( $('<div>',
+        { id: '"jardines"+ncargas+."json"', text: jardines.titulo+" "+jardines.foto+" "+jardines.descripcion+" "+jardines.fecha }));
+
+           }); 
+      }
+
+
+
+      $("ji").click(function() {
+        window.location = $(this).find("a").attr("href"); 
+        return false;
+      });
+
+      $("jr").click(function() {
+        window.location = $(this).find("a").attr("href"); 
+        return false;
+      });
