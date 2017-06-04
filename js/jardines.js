8e9a00c7a14c22382fefcd20e266bab8df8c187e
jsixtvf <jsixtvf@hotmail.com>
2017-06-04 17:48:40 +0200

    Update jardines.js

--- js/jardines.js
+++ js/jardines.js
@@ -38,7 +38,7 @@
            function ponerJardines(json){
                  $.each( json, function( ncargas, jardines ) {
                      $("#vacio").append( $('<div>',
-              { id: '"jardines"+ncargas+".json"', text:jardines.titulo+" "+jardines.foto+" "+jardines.descripcion+" "+jardines.fecha}));
+              { id: '"jardines"+ncargas+".json"', text: jardines.titulo+" "+jardines.foto+" "+jardines.descripcion+" "+jardines.fecha}));
  
                  }); 
             }
