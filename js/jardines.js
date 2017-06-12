4c69cf9c0b05a16af255b87e376c6872275658e4
jsixtvf <jsixtvf@hotmail.com>
2017-06-12 14:21:22 +0200

    Create jardines.js

--- js/jardines.js
+++ js/jardines.js
@@ -21,7 +21,7 @@
 
               function ponerJardines(jsonArray) {
              $.each(jsonArray, function(ncargas, noticia) {
-                 $("#vacio").append($("<div id='noticia"+(i+2)+"'><h1>"+noticia.titulo+
+                 $("#vacio").append($("<div id='noticia"+(ncargas+2)+"'><h1>"+noticia.titulo+
                                  "</h1><p>"+noticia.descripcion+"</p><img src='https://rawgit.com/jsixtvf/jardines/master/"+noticia.foto+"'></div>")        
                         );
 
