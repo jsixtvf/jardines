7a0fe47ffc760033b326a5aec08076c4a6bea38e
jsixtvf <jsixtvf@hotmail.com>
2017-06-12 13:48:13 +0200

    Update jardines.js

--- js/jardines.js
+++ js/jardines.js
@@ -22,7 +22,7 @@
               function ponerJardines(jsonArray) {
              $.each(jsonArray, function(ncargas, noticia) {
                  $("#vacio").append($("<div id='noticia"+(i+2)+"'><h1>"+noticia.titulo+
-                                 "</h1><p>"+noticia.descripcion+"</p><img src='https://rawgit.com/jsixtvf/jardines/master/"+noticia.foto+"'></div>");        
+                                 "</h1><p>"+noticia.descripcion+"</p><img src='https://rawgit.com/jsixtvf/jardines/master/"+noticia.foto+"'></div>")        
                         );
 
                   });
